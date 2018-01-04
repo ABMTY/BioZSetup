c28546333b061c464b4ba82f59d491e9c2daeff0
Alvarad0 <alvarado.programador@gmail.com>
2018-01-04 09:25:04 -0600

    Instalador funcional de reloj checador

---
+++ ServicesBioZ/InstallerSetup.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Configuration;
+using System.Web.Configuration;
+
+namespace ServicesBioZ
+{
+    [RunInstaller(true)]
+    public partial class InstallerSetup : System.Configuration.Install.Installer
+    {
+        public InstallerSetup()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            try
+            {
+                AddConfigurationFileDetails();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Falha ao atualizar o arquivo de configuração da aplicação: " + e.Message);
+                base.Rollback(savedState);
+            }
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+
+        private void showParameters()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringDictionary myStringDictionary = this.Context.Parameters;
+            if (this.Context.Parameters.Count > 0)
+            {
+                foreach (string myString in this.Context.Parameters.Keys)
+                {
+                    sb.AppendFormat("String={0} Value= {1}\n", myString,
+                    this.Context.Parameters[myString]);
+                }
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void AddConfigurationFileDetails()
+        {
+            try
+            {
+
+                string[] Parametros = Context.Parameters["DISPOSITIVO"].Split('/');
+                string Parametro = Parametros[0];
+                string[] Datos = Parametro.Split(';');
+
+                string DEVICE_IP = Datos[0].ToString();
+                string DEVICE_PORT = Datos[1].ToString();
+
+                Set_DateDivice(DEVICE_IP, DEVICE_PORT);
+
+                       
+                string HOST = Datos[2].ToString();
+                string SERVER = Datos[3].ToString();
+                string SERVICE = Datos[4].ToString();
+                string PROTOCOL = Datos[5].ToString();
+                string BD = Datos[6].ToString();
+                string USER = Datos[7].ToString();
+                string PASSWORD = Datos[8].ToString();
+
+                //string CadenaConexion = "IPD=" + DEVICE_IP + "PUerto=" + DEVICE_PORT + ";Host=" + HOST + ";Server=" + SERVER + ";Service=" + SERVICE + ";Protocol=" + PROTOCOL + ";Database=" + BD + ";UID=" + USER + ";Password=" + PASSWORD;
+                //MessageBox.Show("Cadena de Conexion5:" + CadenaConexion);
+                Set_DateDataBase(HOST,SERVER,SERVICE,PROTOCOL,BD,USER,PASSWORD);
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void Set_DateDivice(string DEVICE_IP, string DEVICE_PORT)
+        {
+            // Get the path to the executable file that is being installed on the target computer  
+            string assemblypath = Context.Parameters["assemblypath"];
+            string appConfigPath = assemblypath + ".config";
+
+            // Write the path to the app.config file  
+            XmlDocument doc = new XmlDocument();
+            doc.Load(appConfigPath);
+
+            XmlNode configuration = null;
+            foreach (XmlNode node in doc.ChildNodes)
+                if (node.Name == "configuration")
+                    configuration = node;
+
+            if (configuration != null)
+            {
+                //MessageBox.Show("configuration != null");  
+                // Get the ‘appSettings’ node  
+                XmlNode settingNode = null;
+                foreach (XmlNode node in configuration.ChildNodes)
+                {
+                    if (node.Name == "appSettings")
+                        settingNode = node;
+                }
+
+                if (settingNode != null)
+                {
+                    //MessageBox.Show("settingNode != null");  
+                    //Reassign values in the config file  
+                    foreach (XmlNode node in settingNode.ChildNodes)
+                    {
+                        //MessageBox.Show("node.Value = " + node.Value);  
+                        if (node.Attributes == null)
+                            continue;
+                        XmlAttribute attribute = node.Attributes["value"];
+                        //MessageBox.Show("attribute != null ");  
+                        //MessageBox.Show("node.Attributes['value'] = " + node.Attributes["value"].Value);  
+                        if (node.Attributes["key"] != null)
+                        {
+                            //MessageBox.Show("node.Attributes['key'] = " + node.Attributes["key"].Value);
+
+                            switch (node.Attributes["key"].Value)
+                            {
+                                case "device_ip":
+                                    {
+                                        attribute.Value = DEVICE_IP;
+                                        //MessageBox.Show("node.Attributes['Value'] = " + DEVICE_IP);
+                                    }
+                                    break;
+                                case "device_port":
+                                    {
+                                        attribute.Value = DEVICE_PORT;
+                                        //MessageBox.Show("node.Attributes['Value'] = " + DEVICE_PORT);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+                doc.Save(appConfigPath);
+            }
+
+        }
+
+        private void Set_DateDataBase(string HOST, string SERVER, string SERVICE, string PROTOCOL, string BD, string USER, string PASSWORD)
+        {
+            // Get the path to the executable file that is being installed on the target computer  
+            string assemblypath = Context.Parameters["assemblypath"];
+            string appConfigPath = assemblypath + ".config";
+            
+            //string CadenaConexion = "Data Source ="+ IP_BD +"; Initial Catalog ="+ BD +"; Persist Security Info = True; User ID="+ USER +"; Password="+ PASSWORD;
+            string CadenaConexion = "Host="+ HOST + "; Server="+ SERVER + "; Service="+ SERVICE + "; Protocol="+ PROTOCOL + "; Database="+ BD + "; UID="+ USER + "; Password=" + PASSWORD+";";
+            //MessageBox.Show("Cadena de Conexion:" + CadenaConexion);
+            //updating config file
+            XmlDocument XmlDoc = new XmlDocument();
+
+            //Loading the Config file
+            XmlDoc.Load(appConfigPath);
+            foreach (XmlElement xElement in XmlDoc.DocumentElement)
+            {
+               
+                if (xElement.Name == "connectionStrings")
+                {
+                    //MessageBox.Show("Cadena de Conexion:" + CadenaConexion);
+                    //setting the coonection string
+                    xElement.FirstChild.Attributes[1].Value = CadenaConexion;
+                    //MessageBox.Show("ConecctionString:" + xElement.FirstChild.Attributes[1].Value.ToString());
+                }
+            }
+
+            //writing the connection string in config file
+            XmlDoc.Save(appConfigPath);             
+        }
+
+    }
+}
---
+++ ServicesBioZ/Service1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using System.IO;
+using zkemkeeper;
+using System.Threading;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using ServicesBioZ.Model;
+using System.Net;
+using IBM.Data.Informix;
+
+namespace ServicesBioZ
+{
+    public partial class Service1 : ServiceBase
+    {
+        // private System.Timers.Timer timer1 = null;
+        string filePath = ConfigurationManager.AppSettings["file_log"];
+        bool connSatus = false;
+        CZKEM objCZKEM;
+        string ip = string.Empty;
+        string port = string.Empty;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {           
+           
+            ip = ConfigurationManager.AppSettings["device_ip"];
+            port = ConfigurationManager.AppSettings["device_port"];
+
+            Thread createComAndMessagePumpThread = new Thread(() =>
+            {
+                objCZKEM = new CZKEM();
+                
+                connSatus = Connect_Net(ip, int.Parse(port));
+                if (connSatus)
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine("Machine is connected on" + "Date :" + objCZKEM.MachineNumber.ToString());
+                        writer.WriteLine("Machine is connected on" + "Date :" + DateTime.Now.ToString() + "status" + connSatus);                        
+                        writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                        EventLog.WriteEntry("Machine is connected on" + "Date :" + DateTime.Now.ToString() + "Status: " + connSatus);
+                    }
+                // Ping a Dispositivo
+
+                bool isValidIpA = ValidateIP(ip);
+                if (!isValidIpA)
+                    EventLog.WriteEntry("The Device IP is invalid !!");
+                else
+                    EventLog.WriteEntry("The Device IP is valid !!");
+
+
+                Application.Run();
+
+            });
+            createComAndMessagePumpThread.SetApartmentState(ApartmentState.STA);
+
+            createComAndMessagePumpThread.Start();
+
+            EventLog.WriteEntry("Service BioZ started.");
+        }
+        private bool Connect_Net(string IPAdd, int Port)
+        {
+            if (objCZKEM.Connect_Net(IPAdd, Port))
+            {
+                //65535, 32767
+                if (objCZKEM.RegEvent(1, 32767))
+                {
+                    // [ Register your events here ]
+                    // [ Go through the _IZKEMEvents_Event class for a complete list of events
+                    objCZKEM.OnAttTransactionEx += new _IZKEMEvents_OnAttTransactionExEventHandler(zkemClient_OnAttTransactionEx);
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine("finger print Event is registered... ");
+                        writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                    }                    
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void zkemClient_OnAttTransactionEx(string EnrollNumber, int IsInValid, int AttState, int VerifyMethod, int Year, int Month, int Day, int Hour, int Minute, int Second, int WorkCode)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DBInformix"].ConnectionString;
+            ip = ConfigurationManager.AppSettings["device_ip"];
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(" OnAttTrasactionEx Has been Triggered,Verified OK on" + "Date :" + "Enrollnumber" + EnrollNumber +"|"+ DateTime.Now.ToString());
+                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+            }
+                PerInformixDB per = new PerInformixDB();
+                try
+                {
+                    checkinout entidad = new checkinout();
+                    entidad.anio = Year;
+                    entidad.mes = Month;
+                    entidad.dia = Day;
+                    entidad.hora = Hour;
+                    entidad.minuto = Minute;
+                    entidad.segundo = Second;
+                    entidad.numeroCredencial = int.Parse(EnrollNumber);
+                    entidad.Device = ip;          
+
+                    per.AbrirConexion();
+                    EventLog.WriteEntry("Connection Open");
+
+                    var sql = "insert into checkinout (enrollnumber,date,hour,checkinout,device) values (?,?,?,?,?);";
+
+                    using (var cmd = new IfxCommand(sql, per.Conexion))
+                    {
+                        cmd.Parameters.Add(new IfxParameter()).Value= entidad.numeroCredencial;
+                        cmd.Parameters.Add(new IfxParameter()).Value = entidad.pFecha;
+                        cmd.Parameters.Add(new IfxParameter()).Value = entidad.pHora;
+                        cmd.Parameters.Add(new IfxParameter()).Value = entidad.Ckecked;
+                        cmd.Parameters.Add(new IfxParameter()).Value = entidad.Device;
+                        cmd.ExecuteNonQuery();
+                    }
+                    
+                   EventLog.WriteEntry("Insert EnrollNumber: " + EnrollNumber);
+                }
+                catch (Exception ex)
+                {
+                    EventLog.WriteEntry("Error:"+ex.Message);
+                }
+                finally
+                {
+                    per.CerrarConexion();
+                }
+        }
+        public static bool ValidateIP(string addrString)
+        {
+            IPAddress address;
+            if (IPAddress.TryParse(addrString, out address))
+                return true;
+            else
+                return false;
+        }
+        public bool ClearGLog(CZKEM objZkeeper, int machineNumber)
+        {
+            return objCZKEM.ClearGLog(machineNumber);
+        }
+
+        protected override void OnStop()
+        {
+            // timer1.Enabled = false;
+            this.objCZKEM.OnAttTransactionEx -= new _IZKEMEvents_OnAttTransactionExEventHandler(zkemClient_OnAttTransactionEx);
+            objCZKEM.Disconnect();
+
+        }
+
+       
+    }    
+}
